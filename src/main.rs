7f672667f19c0bc815a2ae14d26875625c9e9554
kurisukun <barroshenriques.chris@gmail.com>
2021-05-19 08:58:14 +0200

    Add arguments for command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 use rusqlite::{Connection, Result};
+use structopt::StructOpt;
 
 mod db;
 
@@ -9,6 +10,27 @@ struct User {
     email: String,
     password: String,
     two_factors: bool,
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "sec_labo2", about = "SEC Labo2: two-factor authentication program using Google Authenticator")]
+struct Options{
+    
+    //arg to login
+    #[structopt(short, long, help = "Argument to proceed a login")]
+    login: bool,
+    //arg to register
+    #[structopt(short, long, help = "Argument to proceed a registration")]
+    register: bool,
+    //arg for username
+    #[structopt(long = "username", default_value = "empty")]
+    username: String,
+    //arg for user password
+    #[structopt(long = "password", default_value = "empty")]
+    password: String,
+    //arg to register
+    #[structopt(short, long, help = "Argument to enable/disable the two factors authentication")]
+    twofactors: bool,
 }
 
 fn main(){
@@ -26,4 +48,7 @@ fn main(){
     for person in person_iter {
         println!("Found person {:?}", person.unwrap());
     }
+
+    let opt = Options::from_args();
+    println!("{:?}", opt);
 }
